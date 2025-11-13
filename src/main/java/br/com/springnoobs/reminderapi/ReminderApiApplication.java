40d8dbcfe30d0a19220a30216b1bfc358cb944f4
Lucas Barbosa Dourado <lucas.dourado@neomind.com.br>
2025-11-13 20:38:07 -0300

    Upload do projeto

---
+++ src/main/java/br/com/springnoobs/reminderapi/ReminderApiApplication.java
@@ -0,0 +1,15 @@
+package br.com.springnoobs.reminderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApiApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ReminderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springnoobs/reminderapi/ReminderApiApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.springnoobs.reminderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderApiApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
